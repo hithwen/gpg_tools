7be3bd54c642df8be1d7c31576988712565ba3c8
Julia Simon <611228+hithwen@users.noreply.github.com>
2020-07-17 13:11:40 +0200

    Remove klembord dependency

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ setuptools.setup(
     install_requires=[
         'pyperclip',
         'python-gnupg',
-        'klembord'
     ],
     python_requires='>=3.7',
     entry_points={"console_scripts": ["realpython=gpgclip.__main__:main"]},
