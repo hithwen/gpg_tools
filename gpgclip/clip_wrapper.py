7c655bd94e22dec0fb4e3fbb6d005e52759469d1
Julia Simon <611228+hithwen@users.noreply.github.com>
2020-07-20 10:24:45 +0200

    Better load clipboard with primary on linux

--- gpgclip/clip_wrapper.py
+++ gpgclip/clip_wrapper.py
@@ -7,11 +7,28 @@ log = logging.getLogger(__name__)
 
 
 class ClipWrapper(object):
-    def _is_linux(self):
+    def __init__(self):
+        self.has_primary = False
+        if self._is_linux():
+            try:
+                pyperclip.set_clipboard('xclip')
+                log.debug("Set clipboard driver to xclip")
+                self.has_primary = True
+            except Exception as xclip_exception:
+                try:
+                    pyperclip.set_clipboard('xsel')
+                    log.debug("Set clipboard driver to xclip")
+                    self.has_primary = True
+                except Exception as xsel_exception:
+                    log.error("Could not select a clipboard driver with primary selection. Errors:\n{}\n{}".format(
+                        xclip_exception, xsel_exception))
+
+    @staticmethod
+    def _is_linux():
         return platform.system() == 'Linux'
 
     def read_primary(self):
-        if self._is_linux():
+        if self.has_primary:
             return pyperclip.paste(primary=True)
         else:
             log.debug("Primary selection does not work in this system, using clipboard instead")
@@ -26,7 +43,7 @@ class ClipWrapper(object):
         pyperclip.copy(message)
 
     def write_primary(self, message):
-        if self._is_linux():
+        if self.has_primary:
             return pyperclip.copy(message, primary=True)
         else:
             log.debug("Primary selection does not work in this system, using clipboard instead")
